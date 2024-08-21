7dbbd71e6b732093f44ec3cb3179827ade05f75d
Peter Hubina <peter.hubina1@gmail.com>
2024-08-21 09:41:38 +0200

    ftSchema update

--- backend/ftSchema.ts
+++ backend/ftSchema.ts
@@ -1,5 +1,12 @@
 import {z} from 'zod';
 
+export const action = () => z.object({
+    access: z.array(z.string()),
+    steps: z.array(z.string()),
+});
+
+export const actions = () => z.record(action());
+
 export const trigger = () => z.object({
     type: z.string(),
     eventType: z.string(),
@@ -11,8 +18,8 @@ export const triggers = () => z.record(trigger());
 
 export const schema = z.object({
     extends: z.string(),
-    triggers: z.array(triggers()),
-    actions: z.array(z.any()).optional(),
+    triggers: z.array(triggers()).optional(),
+    actions: z.array(actions()).optional(),
     stateAttributes: z.array(z.any()).optional(),
     workflow: z.array(z.any()).optional(),
     conditions: z.array(z.any()).optional(),
